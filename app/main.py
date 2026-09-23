413e8f8543b11cd1414227ec5af762ccac220b55
Sasha <oleksandr.novytskyi@gmail.com>
2026-09-23 16:21:34 +0200

    Add project read endpoints

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.schemas import ProjectCreate
 from app.schemas import ProjectResponse
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -33,3 +34,31 @@ def create_project(project:ProjectCreate) -> dict:
     next_project_id += 1
 
     return new_project
+
+
+@app.get("/projects", response_model=list[ProjectResponse])
+def get_projects():
+    return projects
+
+
+@app.get("/projects/{project_id}", response_model=ProjectResponse)
+def get_project(project_id: int):
+    for project in projects:
+        if project["id"] == project_id:
+            return project
+
+    raise HTTPException(
+        status_code=404,
+        detail='Project not found'
+    )
+
+
+def find_project(projects: list[dict], project_id: int) -> dict | None:
+    for project in projects:
+        if project['id'] == project_id:
+            return project
+
+    raise HTTPException(
+        detail='Project not found'
+    )
+
--- app/tests/test_projects.py
+++ app/tests/test_projects.py
@@ -23,6 +23,7 @@ def test_create_project() -> None:
     assert data["description"] == "Testing"
     assert "id" in data
 
+
 def test_create_project_without_name_fails() -> None:
     response = client.post(
         "/projects",
@@ -33,3 +34,43 @@ def test_create_project_without_name_fails() -> None:
     )
 
     assert response.status_code == 422
+
+
+def test_get_projects() -> None:
+    response = client.get("/projects")
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+
+def test_get_project() -> None:
+    create_response = client.post(
+        "/projects",
+        json={
+            "name": "Test project",
+            "description": "Test"
+        },
+    )
+
+    project = create_response.json()
+
+    response = client.get(f"/projects/{project['id']}")
+
+    assert response.status_code == 200
+    assert response.json()["name"] == "Test project"
+
+def test_get_project_404() -> None:
+    create_response = client.post(
+        "/projects",
+        json={
+            "name": "Test project",
+            "description": "Test",
+        },
+    )
+
+    project = create_response.json()
+
+    response = client.get(f"/projects/{10}")
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Project not found"
